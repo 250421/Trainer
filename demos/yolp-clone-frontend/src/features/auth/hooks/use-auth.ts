a3165f6fc26c34685480569b431e6968317dd110
Bao Duong <bao.duong@revature.com>
2025-04-23 13:01:59 -0600

    feat(auth): create Auth interface to define user structure and improve type safety fix(use-auth): update useAuth hook to return Auth type and handle null case feat(routes): implement loading state and redirect in Index component based on user authentication status

--- demos/yolp-clone-frontend/src/features/auth/hooks/use-auth.ts
+++ demos/yolp-clone-frontend/src/features/auth/hooks/use-auth.ts
@@ -1,10 +1,11 @@
 import { axiosInstance } from "@/lib/axios-config";
 import { useQuery } from "@tanstack/react-query";
+import { Auth } from "../models/auth";
 
 export const useAuth = () => {
   return useQuery({
     queryKey: ["auth"],
-    queryFn: async () => {
+    queryFn: async (): Promise<Auth | null> => {
       try {
         const resp = await axiosInstance.get("/auth");
         return resp.data;
