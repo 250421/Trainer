69a06a2a920df0c14bfc2e2891e2b7b51a985062
Bao Duong <bao.duong@revature.com>
2025-04-23 11:58:08 -0600

    feat(package.json): add @tanstack/react-query and @tanstack/eslint-plugin-query dependencies for improved data fetching and linting support feat(__root.tsx): integrate React Query with QueryClientProvider to manage server state and improve data handling in the application

--- demos/yolp-clone-frontend/src/routes/__root.tsx
+++ demos/yolp-clone-frontend/src/routes/__root.tsx
@@ -1,9 +1,25 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-    </>
-  ),
+  component: () => <RootLayout />,
 });
+
+const RootLayout = () => {
+  // Create a client
+  const queryClient = new QueryClient();
+
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
+    </div>
+  );
+};
