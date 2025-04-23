9da05612042cd07c0a14f50cf4163f42e7d4863c
Bao Duong <bao.duong@revature.com>
2025-04-23 11:50:33 -0600

    feat(sign-up): add sign-up form with validation and axios configuration for API requests feat(package): add axios dependency for making HTTP requests feat(axios-config): create axios instance with base URL and default headers feat(sign-up-schema): create Zod schema for sign-up form validation

---
+++ demos/yolp-clone-frontend/src/features/auth/schemas/sign-up-schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+  username: z.string().min(1, {
+    message: "Username is required",
+  }),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
+});
+
+export type SignupSchemaType = z.infer<typeof signupSchema>;
--- demos/yolp-clone-frontend/src/routes/(public)/_public.sign-up.tsx
+++ demos/yolp-clone-frontend/src/routes/(public)/_public.sign-up.tsx
@@ -1,9 +1,96 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  signupSchema,
+  SignupSchemaType,
+} from "@/features/auth/schemas/sign-up-schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export const Route = createFileRoute("/(public)/_public/sign-up")({
   component: SignUpPage,
 });
 
 function SignUpPage() {
-  return <div>Hello "/(public)/_public/sign-up"!</div>;
+  const form = useForm<SignupSchemaType>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: SignupSchemaType) {
+    console.log(values);
+  }
+
+  return (
+    <Card className="w-[400px]">
+      <CardHeader>
+        <CardTitle className="font-bold text-2xl">Sign Up</CardTitle>
+        <CardDescription>Please fill in the details below:</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+
+        <div className="flex items-center gap-x-2 pt-4">
+          <p>Already have an account?</p>
+          <Link to={"/sign-in"} className="text-blue-500 underline">
+            Sign In
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
