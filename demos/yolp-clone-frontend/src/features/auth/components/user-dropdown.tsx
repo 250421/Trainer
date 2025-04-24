606ba72b2791a7964e63759dc5a6044ac3d795c4
Bao Duong <bao.duong@revature.com>
2025-04-24 12:43:32 -0600

    feat(confirm-dialog): add confirmation dialog component for user actions feat(user-dropdown): integrate confirmation dialog for logout action to enhance user experience feat(use-confirm): create custom hook for managing confirmation dialogs in the application

--- demos/yolp-clone-frontend/src/features/auth/components/user-dropdown.tsx
+++ demos/yolp-clone-frontend/src/features/auth/components/user-dropdown.tsx
@@ -6,18 +6,36 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { UserProfile } from "./user-profile";
 import { useSignOut } from "../hooks/use-sign-out";
+import { useConfirm } from "@/hooks/use-confirm";
 
 export const UserDropdown = () => {
+  const [logOutConfirm, LogOutDialog] = useConfirm();
+
   const { mutate: signOut } = useSignOut();
 
+  const handleLogOut = async () => {
+    const ok = await logOutConfirm();
+    if (!ok) return;
+
+    signOut();
+  };
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <UserProfile />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => signOut()}>Log out</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <UserProfile />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={handleLogOut}>Log out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <LogOutDialog
+        title={"Log Out"}
+        description={"Are you sure you want to log out?"}
+        destructive
+      />
+    </>
   );
 };
